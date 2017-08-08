0880724f629525375b70393553c3e498de4a8434
ibrah <ibrahimkb1996@gmail.com>
2017-08-08 22:32:23 +0530

    Update

--- send_notification_android.java
+++ send_notification_android.java
@@ -9,10 +9,15 @@ mRootref.child("Users").child(mChatuser).child("online").addListenerForSingleVal
 				
                                 if(!online.equals("true")){
 
+
+				//Uid is the id of current logged in user sending the message
+
                                     Map notimap = new HashMap();
                                     notimap.put("from",Uid);
                                     notimap.put("type","message");
                                     notimap.put("message",message);
+					
+				//Message is uploaded to child "MessageNoti" of which we set a onwrite trigger function in JS.
 
                                     mRootref.child("MessageNoti").child(mChatuser).push().setValue(notimap)
                                             .addOnSuccessListener(new OnSuccessListener<Void>() {
